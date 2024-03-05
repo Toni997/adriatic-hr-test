3b69192b4fe6239c0dca39433ad37ba0d8092f8f
Toni Kazinoti <skuezyt@gmail.com>
2024-03-05 02:03:19 +0100

    production test log

--- src/app/components/reservation/ReservationConfirmationDetails.tsx
+++ src/app/components/reservation/ReservationConfirmationDetails.tsx
@@ -11,6 +11,7 @@ const ReservationConfirmationDetails = () => {
   const router = useRouter()
 
   useEffect(() => {
+    console.log('working?', reservationDetails)
     if (!reservationDetails) router.push('/')
   }, [])
 
